3c6fd864a7e277c5e862e29feb05feff06f5b59f
Benjamin J. Balter <ben@balter.com>
2012-12-31 12:58:23 -0500

    remove limit related logic from front end, implemented in API. Defaults to 2. Change in discovery/app/controllers/page.rb

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -8,7 +8,6 @@ window.MyGovBar={
     api_url: '{{ site.api_url }}',
     version: {{ site.version }},
     tabs: [{% for tab in site.tabs %}'{{tab}}'{%if forloop.rindex0 > 0 %},{%endif%}{%endfor%}],
-    search_affiliate: '{{ site.search_affiliate }}',
-    max_related: {{ site.max_related }}
+    search_affiliate: '{{ site.search_affiliate }}'
   }
 };
