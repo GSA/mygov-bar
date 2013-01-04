84adaa0a78caca46dfd91fff129e90415ef36026
dannychapman <dchapman02@mac.com>
2013-01-04 16:24:08 -0500

    styled animated buttons, fixed padding.

--- _includes/js/bookmarklet.js
+++ _includes/js/bookmarklet.js
@@ -1,2 +1,2 @@
-/*! MyGovBar — 0.0.1 — 2013-01-04 15:59 */
+/*! MyGovBar — 0.0.1 — 2013-01-04 15:17 */
 (function(){var t;t=document.createElement("script"),t.src="{{ site.url }}/embed-code.js",document.body.appendChild(t)}).call(this);
